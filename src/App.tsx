28445c73d358899c5ecfac6f6868ee18d7346a9a
JyojiTenguam <j.y.oji@hotmail.com>
2024-04-24 10:12:01 -0300

    Resolvendo requisito 03 de Exercise ghibli animations ts

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
   const contextValue = {
     films,
     favorites,
+    toggleFavorite,
   };
 
   return (
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,27 @@
+// src/pages/Favorites.tsx
+import { useContext } from 'react';
+import FilmCard from '../components/FilmCard';
+import Header from '../components/Header';
+import FilmsContext from '../context/FilmsContext';
+
 export default function Favorites() {
+  const { favorites } = useContext(FilmsContext);
+
   return (
-    <h1>Favorites</h1>
+    <>
+      <Header />
+      <main>
+        <h1>Favorites</h1>
+        <section>
+          {favorites.map((film) => (
+            <FilmCard
+              key={ film.id }
+              film={ film }
+              isFavorite={ favorites.some((favorite) => favorite.id === film.id) }
+            />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
